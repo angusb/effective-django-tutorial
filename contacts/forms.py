f0b83ac06d1c2449d6c201b11a1f0478c7b71638
Nathan Yergler <nathan@yergler.net>
2013-02-24 11:01:12 -0800

    View for editing addresses for a Contact.

---
+++ contacts/forms.py
@@ -0,0 +1,14 @@
+from django.forms.models import inlineformset_factory
+
+from contacts.models import (
+    Contact,
+    Address,
+)
+
+
+# inlineformset_factory creates a Class from a parent model (Contact)
+# to a child model (Address)
+ContactAddressFormSet = inlineformset_factory(
+    Contact,
+    Address,
+)
--- contacts/views.py
+++ contacts/views.py
@@ -8,6 +8,7 @@ from django.views.generic import (
 )
 
 from contacts.models import Contact
+import forms
 
 
 class ListContactView(ListView):
@@ -62,3 +63,15 @@ class DeleteContactView(DeleteView):
 
     def get_success_url(self):
         return reverse('contacts-list')
+
+
+class EditContactAddressView(UpdateView):
+
+    model = Contact
+    template_name = 'edit_addresses.html'
+    form_class = forms.ContactAddressFormSet
+
+    def get_success_url(self):
+
+        # redirect to the Contact view.
+        return self.get_object().get_absolute_url()
